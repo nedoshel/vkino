f12e7fc0c7eff8605a4ddd3f71faa2bb1454ac0d
nedoshel <nedoshel@gmail.com>
2014-01-31 15:15:53 +0400

    blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vkino::Application.config.secret_key_base = '425908339b4a4eecaa023b67b304876cb30b5c57b876f9040f46c3af51b8dfc41f0d80a55bbb5c0be37cb93fbaeea387347c438e3f5714bc9b8a9d26e8bc04a9'
