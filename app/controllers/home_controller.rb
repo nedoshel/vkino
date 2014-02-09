7c54a5ad50e6581092e23d8f9266265257c2ce7e
nedoshel <nedoshel@gmail.com>
2014-02-09 18:15:17 +0400

    актеры, режиссеры, паджинация

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @films = Film.joins(:views).distinct.has_image.order(:views_count).reverse_order
+    @films = Film.viewed.distinct.has_image.order(:views_count).reverse_order
   end
 end
